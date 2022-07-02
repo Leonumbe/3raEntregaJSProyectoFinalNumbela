1d0a316ed2591d6abcbab83e254c3d36b6345977
unknown <leonumbela@gmail.com>
2022-07-02 17:33:23 -0300

    Se agrega campos de selector de fecha, se trabaja css sobre los mismos, se agrega funciones varias, resta cotejar validaciones

--- scripts/pages.js
+++ scripts/pages.js
@@ -3,6 +3,7 @@ let reserva;
 let deleteCart;
 let userBooked;
 let buttonSubmit;
+let fee = 0.1;
 
 //Nuestra reserva la guardamos en el localStorage
 if (JSON.parse(localStorage.getItem('reserva'))){
@@ -32,7 +33,7 @@ if(reserva.length == 0){
     body.innerHTML += noBookedSelection; 
     }else{
     const summary = `
-    <h1 class="subT">Booked Selection</h1>
+    <h1 id="bookedSelection"class="subT  h1adapted">Booked Selection</h1>
     <div id='tBody'></div>
     <a id="filter" class="btn" href="../index.html">Back</a>
     <a href="#Form" class="btn">End Booked</a>
@@ -43,15 +44,22 @@ if(reserva.length == 0){
         const element = reserva[i];
         const { id, category, name, price, img, cantidad } = element;
         const reservaFinal = `
-        <div id=${id}>
-            <img src="${img}" alt="Imagen de la room">    
-            <div class="head">${name}</div>
-            <div class="subT">${category}</div>
-            <div>${cantidad}</div>
-            <div class="price">$${price.toLocaleString()}- Euros</div>
-            <div>Precio*Q: $${(cantidad * price).toLocaleString()}</div>
+        <div class='cardBooked'>
+            <div class='box'>
+        <div id=${id} >
+            <img src="${img}" alt="Imagen de la room"> 
+            <div class='boxBooked'>
+            <div class="head">Name: ${name}</div>
+            <div class="subT">Category: ${category}</div>
+            <div>Quantity booked: ${cantidad}</div>
+            <div class="price">Price per night: $${price.toLocaleString()}- Euros</div>
+            <div>Total Amount: $ ${(cantidad * price * localStorage.getItem("dateTotal")).toLocaleString()}- Euros</div>
             <button id='delete' class='btn'>Delete</button>
-        </div>`
+        </div>
+        </div>
+        </div>
+        </div>
+        `
         tbody.innerHTML += reservaFinal
     }
 
@@ -62,6 +70,16 @@ if(reserva.length == 0){
         reserva = []
         tbody.innerHTML = ``
         localStorage.setItem('reserva', JSON.stringify(reserva))
+        checkIn = '';
+        checkOut = '';
+        dateTotal = '';
+        pasajeroSelect = ''
+        userBooked = {};
+        localStorage.setItem('check-in',JSON.stringify(checkIn))
+        localStorage.setItem('check-out',JSON.stringify(checkOut))
+        localStorage.setItem('dateTotal',JSON.stringify(dateTotal))
+        localStorage.setItem('pasajeros',JSON.stringify(pasajeroSelect))
+        localStorage.setItem('userBooked', JSON.stringify(userBooked))
         console.log(reserva)
     }
 }
@@ -118,5 +136,87 @@ buttonSubmit = document.getElementById('buttonSubmit');
 buttonSubmit.onclick = (e) => {
     e.preventDefault()
     endBooked()
+    Swal.fire({
+        title: 'Great, your booked were processed correctly!!!',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      })
 }
+
+//-----------------------check-in-out-------------------------------//
+
+//Cargo guests
+let guests = [ '', 1, 2];
+let pasajeros = document.getElementById('pasajeros'); 
+
+function opciones(arrayGuests, opcion){
+    let guestsChoose = "";
+    for (let i = 0; i <arrayGuests.length; i++){
+        guestsChoose += "<option>" + arrayGuests[i] + "</option>";
+    }
+    opcion.innerHTML = guestsChoose
+} 
+opciones(guests, pasajeros)
+
+//Guests select
+function obtenerPasajero(){
+    let pasajeroSelect = document.getElementById("pasajeros");
+    localStorage.setItem(pasajeroSelect.id, pasajeroSelect.value)
+}
+
+Date
+let actualDate;
+function fechaActualizada(){
+    actualDate = new Date();
+    console.log(actualDate.getTime())
+}
+fechaActualizada()
+
+//Date
+let checkIn = document.getElementById("check-in");
+let checkOut = document.getElementById("check-out");
+
+function obtenerFecha(){   
+    localStorage.setItem(checkIn.id, checkIn.value);
+    localStorage.setItem(checkOut.id, checkOut.value);
+   
+    let checkInDate = new Date(checkIn.value);
+    let checkOutDate = new Date(checkOut.value);
+    //console.log(checkInDate.getTime())
+
+    if (checkInDate.getTime() < actualDate.getTime()) {
+       alert("error, debe elegir una fecha vigente para su checkin"+"\n"+actualDate)
+        checkInDate = new Date(checkIn.value);
+    }if((checkOutDate <= actualDate)||(checkOutDate == checkInDate )) {
+       alert("error, debe elegir una fecha posterior a la fecha de checkin"+"\n"+checkInDate)
+        checkOutDate = new Date(checkIn.value);
+    }else{
+    let dateTotal = checkOutDate - checkInDate;
+    localStorage.setItem("dateTotal", Math.floor(dateTotal / (1000 * 60 * 60 * 24)));
+    alert("Ha reservado esta habitacion por "+dateTotal/(1000 * 60 * 60 * 24)+" noche/s")
+    }
+    //ver validacion
+    // if (localStorage.getItem(dateTotal) == NaN) {
+    //     alert("debe definir fechas")
+    // } else {
+    //     alert("Ha reservado esta habitacion por "+dateTotal/(1000 * 60 * 60 * 24)+" noche/s") 
+    // }
+    
+}
+
+function pasajeroDatos(){
+    obtenerPasajero()
+    obtenerFecha()
+  
+    if(checkIn.value,checkOut.value,pasajeros.value === null || checkIn.value,checkOut.value,pasajeros.value === ''){
+        alert("Select a date and guests to continue")
+    }else{
+        window.location.href = "#bookedInProgress";
+    }
+}
+
 
